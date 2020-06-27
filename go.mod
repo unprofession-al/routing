7fb921bb01ddc1db2062e93802f7c2aecf53edfa
Daniel Menet <membership@sontags.ch>
2020-06-27 12:22:21 +0200

    improved doc output

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/unprofession-al/routing
 
+go 1.14
+
 require (
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
--- openapi.go
+++ openapi.go
@@ -1,7 +1,81 @@
 package router
 
-import yaml "gopkg.in/yaml.v2"
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"strings"
+)
 
-func (r Route) Definition() ([]byte, error) {
-	return yaml.Marshal(r)
+const tplString = `<div class="{{ .Class }}"><table>
+	<tr>
+		<th>Path</th>
+		<th>Method</th>
+		<th>Description</th>
+		<th>Query Parameters</th>
+	</tr>
+{{ range $e := .Endpoints }}
+	<tr>
+		<td>{{ $e.Path }}</td>
+		<td>{{ $e.Method }}</td>
+		<td>{{ $e.Description }}</td>
+		<td>
+{{ range $q := $e.QueryParams }}
+			<b>Name: {{ $q.N }} | Default: {{ $q.D }}</b></br>
+			<p>{{ $q.Desc }}</p></br>
+{{ end }}
+		</td>
+	</tr>
+{{ end }}
+</table></div>
+`
+
+type RouteDoc struct {
+	Path        string
+	Method      string
+	Description string
+	QueryParams []*QueryParam
+}
+
+func (r Route) flatten(doc *[]RouteDoc, base string) {
+	base = fmt.Sprintf("/%s/", strings.Trim(base, "/"))
+
+	if strings.HasSuffix(base, "*/") {
+		base = strings.TrimSuffix(base, "*/")
+		for m, h := range r.H {
+			rd := RouteDoc{Path: base, Method: m, Description: h.D, QueryParams: h.Q}
+			*doc = append(*doc, rd)
+		}
+		return
+	}
+
+	for m, h := range r.H {
+		rd := RouteDoc{Path: base, Method: m, Description: h.D, QueryParams: h.Q}
+		*doc = append(*doc, rd)
+	}
+
+	for path, route := range r.R {
+		path = fmt.Sprintf("%s%s/", base, strings.Trim(path, "/"))
+		route.flatten(doc, path)
+	}
+}
+
+type TemplateData struct {
+	Class  string
+	Routes []RouteDoc
+}
+
+func (r Route) AsHTML(htmlclass, base string) ([]byte, error) {
+	rd := []RouteDoc{}
+	r.flatten(&rd, base)
+
+	td := TemplateData{
+		Class:  htmlclass,
+		Routes: rd,
+	}
+
+	out := &bytes.Buffer{}
+	tpl := template.Must(template.New("html").Parse(tplString))
+	err := tpl.Execute(out, td)
+	return out.Bytes(), err
 }
